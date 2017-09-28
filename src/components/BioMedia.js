458e84ffec97b5783b00951ee539708a54e63fd2
sMorm <sereymorm@gmail.com>
2017-09-27 22:49:57 -0400

    Migrated images to S3

--- src/components/BioMedia.js
+++ src/components/BioMedia.js
@@ -1,47 +1,26 @@
-// Frontend
-import HTML from '../media/html.png';
-import CSS from '../media/css.png';
-import RLOGO from '../media/react.svg'
-import REDUX from '../media/redux.jpg'
-
-// Backend
-import EXPRESS from '../media/express.jpg'
-import NODE from '../media/nodejs.jpg';
-import APACHE from '../media/apache.jpg'
-import MONGO from '../media/mongo.jpg'
-
-// Other
-import DOCKER from '../media/docker.png'
-import JENKINS from '../media/jenkins.png'
-import AWS from '../media/aws.png'
-import CPP from '../media/cpp.jpg'
-
-// Social
-import GITHUB from '../media/github.png'
-import LINKEDIN from '../media/linkedin.png'
-import STACKOVERFLOW from '../media/stackoverflow.png'
+const s3bucket = 'https://s3.amazonaws.com/sereymorm.com/media/'
 
 const frontEnd = {
   title: "Front-End",
   items: [
     {
       link: "https://en.wikipedia.org/wiki/HTML5",
-      src: HTML,
+      src: `${s3bucket}html.png`,
       alt: "HTML logo"
     },
     {
       link: "https://facebook.github.io/react/",
-      src: RLOGO,
+      src: `${s3bucket}react.svg`,
       alt:"React logo" 
     },
     {
       link: "http://redux.js.org",
-      src: REDUX,
+      src: `${s3bucket}redux.jpg`,
       alt: "Redux Logo" 
     },
     {
-      link: "https://en.wikipedia.org/wiki/CSS3",
-      src: CSS,
+      link: "https://en.wikipedia.org/wiki/CSs3bucket",
+      src: `${s3bucket}css.png`,
       alt: "CSS logo"
     },
   ]
@@ -52,22 +31,22 @@ const backEnd = {
   items: [
     {
       link: "https://www.mongodb.com",
-      src: MONGO,
+      src: `${s3bucket}mongo.jpg`,
       alt: "MongoDB logo" 
     },
     {
       link: "https://nodejs.org/",
-      src: NODE,
+      src: `${s3bucket}nodejs.jpg`,
       alt:"Node logo" 
     },
     {
       link: "https://expressjs.com",
-      src: EXPRESS,
+      src: `${s3bucket}express.jpg`,
       alt: "Express logo"
     },
     {
       link: "https://httpd.apache.org",
-      src: APACHE,
+      src: `${s3bucket}apache.jpg`,
       alt: "Apache logo"
     },
   ]
@@ -78,22 +57,22 @@ const other = {
   items: [
     {
       link: "https://www.docker.com/what-docker",
-      src: DOCKER,
+      src: `${s3bucket}docker.png`,
       alt: "Docker logo"
     },
     {
       link: "https://jenkins.io",
-      src: JENKINS,
+      src: `${s3bucket}jenkins.png`,
       alt:"Node logo" 
     },
     {
       link: "https://aws.amazon.com",
-      src: AWS,
+      src: `${s3bucket}aws.png`,
       alt: "AWS logo" 
     },    
     {
       link: "http://www.cplusplus.com",
-      src: CPP,
+      src: `${s3bucket}cpp.jpg`,
       alt: "C++ logo"
     },
   ]
@@ -104,17 +83,17 @@ const social = {
   items: [
     {
       link: "https://github.com/sMorm",
-      src: GITHUB,
+      src: `${s3bucket}github.png`,
       alt: "Github logo"
     },
     {
       link: "https://www.linkedin.com/in/sereymorm",
-      src: LINKEDIN,
+      src: `${s3bucket}linkedin.png`,
       alt:"LinkedIn logo" 
     },
     {
       link: "https://stackoverflow.com/users/3411861/serey/",
-      src: STACKOVERFLOW,
+      src: `${s3bucket}stackoverflow.png`,
       alt: "stackoverflow logo" 
     }
   ]
