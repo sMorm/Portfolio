549832e1c07316fee4de15c44e58f36660ba043e
sMorm <sereymorm@gmail.com>
2017-09-27 22:58:17 -0400

    Updated Social

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,37 +1,27 @@
 import React from 'react';
 
-import GITHUB from '../media/github.png'
-import LINKEDIN from '../media/linkedin.png'
-import STACKOVERFLOW from '../media/stackoverflow.png'
 import './styles/Social.css'
+
+const s3bucket = 'https://s3.amazonaws.com/sereymorm.com/media/'
+
 const Social = () => {
   return (
-
     <div className="bioContainer logos">
       <h1>Social</h1>
       <hr/>
-        <div className="bioLogos social">
+        <div className="bioLogos">
         <a href="https://github.com/sMorm" target="_blank">
-          <img src={GITHUB} alt="Github logo"/>
+          <img src={`${s3bucket}github.png`} alt="Github logo"/>
         </a>
         <a href="https://www.linkedin.com/in/sereymorm" target="_blank">
-          <img src={LINKEDIN} alt="LinkedIn logo"/>
+          <img src={`${s3bucket}linkedin.png`} alt="LinkedIn logo"/>
         </a>
         <a href="https://stackoverflow.com/users/3411861/serey/" target="_blank">
-          <img src={STACKOVERFLOW} alt="stackoverflow logo"/>
+          <img src={`${s3bucket}stackoverflow.png`} alt="stackoverflow logo"/>
         </a>
         </div>
     </div>
-  );    
+  )    
 }
 
 export default Social;
-
-    // <div className="socialContainer">
-    //   <a href='https://github.com/sMorm' target='_blank' >
-    //     <img src={Github}  alt='GitHub'/>
-    //   </a>
-    //   <a href='https://linkedin.com/in/sereymorm' target='_blank'>
-    //     <img src={Linkedin}  alt='GitHub'/>
-    //   </a>
-    // </div>
